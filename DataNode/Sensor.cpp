f04148b8a035d60a8be80d0e3f5de0465edb9940
insalt-glitch <nils.nmueller@gmail.com>
2021-01-13 09:09:07 +0100

    Optimized a bit for power consumtion. The sensor is now low(power off) until in use.

--- DataNode/Sensor.cpp
+++ DataNode/Sensor.cpp
@@ -17,26 +17,12 @@ void TH_Sensor::Startup(void) {
     digitalWrite(16, LOW);
     digitalWrite(17, LOW);
     // setup our pin
-    /*TODO(Nils): We shouldnt have to write that high all the time.
-      not doing so will save power and we can probably just wait
-      some time and treat the sensor like it was always on.
-
-      I think can replace this with
-      pinMode(DATA_PIN, OUTPUT);
-      digitalWrite(DATA_PIN, LOW);
-    */
-    pinMode(DATA_PIN, INPUT);
-    digitalWrite(DATA_PIN, HIGH);
+    pinMode(DATA_PIN, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
 }
 
-/*
- * TODO(Nils): Go recheck if that makes any sense, since we sometimes
- * get false values. Then we should maybe just try again and select the answer
- * we get most out of the once we have recieved.
- */
 bool TH_Sensor::get_temp_humid(float* temp_humdity_vals) {
-    // try to get the data from the sensor
-    // TODO(Nils) implement a sensor startup here to save power (see above)
+    // try to get the data from the sensor if we fail we return false
     for (uint8_t i=0; !sensorReadout(); i++) {
         if (i > 2) {
             return false;
@@ -44,24 +30,17 @@ bool TH_Sensor::get_temp_humid(float* temp_humdity_vals) {
         delay(2000);
     }
     // temperature is in data[2] and humidity in data[0]
-    // copy temperature and humidity values
     temp_humdity_vals[0] = _data[2];   // stores temperature in data[0]
     temp_humdity_vals[1] = _data[0];   // stores humidty in data[1]
     return true;
 }
 
-/*TODO(Nils):
- * We might want to set counter to a higher number and then change its type to INT, since this
- * could potenially improve the success-rate of our reading process (should kind of depent on the CPU).
- */
 boolean TH_Sensor::sensorReadout(void) {
     boolean laststate = HIGH;  // We use this to detect the bitstates
     uint8_t counter = 0;       // counts time while reading the bits
     uint8_t i = 0;             // index
     uint8_t j = 0;             // index
 
-    // TODO(Nils): I assume this is enough for the sensor to startup
-    // so we dont need a the data_pin high before
     digitalWrite(DATA_PIN, HIGH);
     delay(250);
 
@@ -96,35 +75,37 @@ boolean TH_Sensor::sensorReadout(void) {
     pinMode(DATA_PIN, INPUT);
     // loop for as many bits as we get from the Sensor
     for (i = 0; i < MAXTIMINGS; i++) {
-        counter = 0;
-        // wait for a state change from the input
-        while (digitalRead(DATA_PIN) == laststate) {
-            counter++;
-            delayMicroseconds(1);
-            if (counter == 255) {
-                break;
-            }
-        }
-        laststate = digitalRead(DATA_PIN);
+      counter = 0;
+      // wait for a state change from the input
+      while (digitalRead(DATA_PIN) == laststate) {
+        counter++;
+        delayMicroseconds(1);
         if (counter == 255) {
-            break;
+          break;
         }
-        // ignore first 3 transitions
-        if ((i >= 4) && (i % 2 == 0)) {
-            // shove each bit into the storage bytes
-            _data[j / 8] <<= 1;
-            if (counter > _count) {
-                _data[j / 8] |= 1;
-            }
-            j++;
+      }
+      laststate = digitalRead(DATA_PIN);
+      if (counter == 255) {
+        break;
+      }
+      // ignore first 3 transitions
+      if ((i >= 4) && (i % 2 == 0)) {
+        // shove each bit into the storage bytes
+        _data[j / 8] <<= 1;
+        if (counter > _count) {
+          _data[j / 8] |= 1;
         }
+        j++;
+      }
     }
-    // allow system interrupts from here on out
+    // allow system interrupts from here on out and cut power from sensor
     sei();
+    pinMode(DATA_PIN, OUTPUT);
+    digitalWrite(DATA_PIN, LOW);
+
     // check that we read all 40 bits and that the checksum is correct
     int check_sum = (_data[0] + _data[1] + _data[2] + _data[3]) & 0xFF;
     if ((j >= 40) && (_data[4] == check_sum)) {
-        Serial.print("Success");
         return true;
     }
 
